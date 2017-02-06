3ec4f13452276023681d228c6d9c6fedcc325862
Daniel Jackson <danj210@hotmail.com>
2017-02-05 21:13:46 -0500

    Created small calculator program dor assignment 2.

---
+++ CalculatorApplication/CalculatorApplication/CalculatorRun.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication {
+    class CalculatorRun {
+        static void Main(string[] args) {
+
+            Calculator userCalc = new Calculator();
+            Console.Write("Please enter first opperand: ");
+            userCalc.op1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Please enter second opperand: ");
+            userCalc.op2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("The end result is: ",
+                userCalc.addition());
+            // Pause at the end of the application
+            Console.ReadLine();
+        }
+    }
+    class Calculator {
+        private double opperand1;
+        private double opperand2;
+        private double result;
+        public Calculator() {
+
+        }
+        // Optional second constructor
+        public Calculator(double var1, double var2) {
+            opperand1 = var1;
+            this.opperand2 = var2;
+        }
+        public double op1 {
+            get {
+                return opperand1;
+            }
+            set {
+                opperand1 = value;
+            }
+        }
+        public double op2 {
+            get {
+                return opperand2;
+            }
+            set {
+                opperand2 = value;
+            }
+        }
+        // One way of doing it
+        public double addition() {
+            return result = opperand1 + opperand2;
+        }
+        // Another way of doing it
+        public double addition2() {
+            result = opperand1 + opperand2;
+            return result;
+        }
+    }
+}
