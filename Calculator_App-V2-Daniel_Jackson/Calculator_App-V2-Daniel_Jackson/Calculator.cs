12daf0e993a5d8ba609a0067cd74430ea5f64a0a
Daniel Jackson <danj210@hotmail.com>
2017-02-21 22:32:11 -0500

    Added do while loop and comments.

--- Calculator_App-V2-Daniel_Jackson/Calculator_App-V2-Daniel_Jackson/Calculator.cs
+++ Calculator_App-V2-Daniel_Jackson/Calculator_App-V2-Daniel_Jackson/Calculator.cs
@@ -3,45 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+// Daniel Jackson - Assignment 4 - Calculator App Version 2
 namespace Calculator_App_V2_Daniel_Jackson {
     class Calculator {
+        /*
+        Playing with protected static variable which I know is not best practice.
+        Instructed to make only one file or I would have made a seperate class like
+        in previous assignments.
+        */
         protected static double operator1;
         protected static double operator2;
         protected static string operand;
-        private static double result;
+        protected static double result;
+        protected static bool done = false;
         static void Main(string[] args) {
-            Console.Write( "Please Enter First Operator: ");
+            bool test;
+            // Do while loop runs while boolean variable done = false
+            do {
+                Console.Write("Please Enter First Operator: ");
 
-            operator1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Please enter math: ");
+                operator1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Please enter math: ");
 
-            operand = Console.ReadLine();
-            Console.Write("Please enter second operator: ");
+                operand = Console.ReadLine();
+                Console.Write("Please enter second operator: ");
 
-            operator2 = Convert.ToDouble(Console.ReadLine());
-
-            switch (operand) {
-                case "+":
-                    result = addition(operator1, operator2);
-                    break;
-                case "-":
-                    result = subtraction(operator1, operator2);
-                    break;
-                case "*":
-                    result = multiplication(operator1, operator2);
-                    break;
-                case "/":
-                    result = division(operator1, operator2);
-                    break;
-                default:
-                    Console.WriteLine("You did not enter a correct operator, no calculation done.");
-                    break;
-            }
+                operator2 = Convert.ToDouble(Console.ReadLine());
+                // Switch statement sets bool variable done = true if successful.
+                switch (operand) {
+                    case "+":
+                        result = addition(operator1, operator2);
+                        done = true;
+                        break;
+                    case "-":
+                        result = subtraction(operator1, operator2);
+                        done = true;
+                        break;
+                    case "*":
+                        result = multiplication(operator1, operator2);
+                        done = true;
+                        break;
+                    case "/":
+                        result = division(operator1, operator2);
+                        done = true;
+                        break;
+                    default:
+                        Console.WriteLine("You did not enter a correct operator, no calculation done.");
+                        break;
+                }
+            } while (!done);
+            
             Console.WriteLine("Your result is: " + operator1 + " "
                 + operand + " " + operator2 + " = " + result);
         }
-
+        // Public static members
+        // Static only used because I'm not creating a seperate class to instantiate an object.
         public static double addition(double x, double y) {
             double result = x+y;
             return result;
