f37e68e2f67f1bd58bb6645369d43cd8f799715b
Daniel Jackson <danj210@hotmail.com>
2017-03-06 19:11:58 -0500

    Adding the uppercase method as a library class.

--- Assignment 6 - LINQ/Assignment 6 - LINQ/Program.cs
+++ Assignment 6 - LINQ/Assignment 6 - LINQ/Program.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// I made a seperate solution that could be a portable class library which saves the uppercase
+// method that my professor told me to save for use anywhere that I attach this library to.
+// I can copy the project folder to the class library to anywhere and reference it to use it over again.
+using DanielsClassLibrary;
 
 namespace Assignment_6___LINQ {
     class Program {
         static void Main(string[] args) {
+            List<string> items = new List<string>();
+
+            Console.Write("Please enter first name: ");
+            items.Add(Console.ReadLine());
+            Console.Write("Please enter last name: ");
+            items.Add(Console.ReadLine());
+            Console.Write("Please enter street address: ");
+            items.Add(Console.ReadLine());
+            Console.Write("Please enter city: ");
+            items.Add(Console.ReadLine());
+            Console.Write("Please enter state: ");
+            items.Add(Console.ReadLine());
+            Console.Write("Please enter zip code: ");
+            items.Add(Console.ReadLine());
+
+            var newList = from item in items
+                          let upperCaseWord = DanielsClassLibrary.UpperCaseFirstLetterOfWords.UppercaseWords(item)
+                          select upperCaseWord;
+            foreach (string item in newList) {
+                Console.WriteLine(item);
+            }
         }
+        // Static method to uppercase the first letter of a word.
+        
     }
 }
---
+++ Assignment 6 - LINQ/DanielsClassLibrary/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanielsClassLibrary {
+    public class UpperCaseFirstLetterOfWords {
+        public static string UppercaseWords(string value) {
+            char[] array = value.ToCharArray();
+
+            if (array.Length >= 1) {
+                if (char.IsLower(array[0])) {
+                    array[0] = char.ToUpper(array[0]);
+                }
+            }
+            for (int i = 1; i < array.Length; i++) {
+                if (array[i - 1] == ' ') {
+                    if (char.IsLower(array[i])) {
+                        array[i] = char.ToUpper(array[i]);
+                    }
+                }
+            }
+            return new string(array);
+        }
+    }
+}
