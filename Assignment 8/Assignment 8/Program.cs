efe30dd10dd4ea015adcc632b3579a00d35df5ee
Daniel Jackson <danj210@hotmail.com>
2017-03-25 22:25:07 -0400

    Full assignment

---
+++ Assignment 8/Assignment 8/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7 {
+    class Program {
+        static void Main(string[] args) {
+            List<Student> myList = new List<Student>();
+
+            Student s1 = new Student();
+            s1.firstName = "John";
+            s1.lastName = "Smith";
+            s1.studentId = 2560;
+            myList.Add(s1);
+
+            Student s2 = new Student("Peter");
+            myList.Add(s2);
+
+            Student s3 = new Student("Morgan", "Simmons");
+            myList.Add(s3);
+
+            Student s4 = new Student("James", "Walters");
+            myList.Add(s4);
+
+            Student s5 = new Student("Linda", "Scott", 1005);
+            myList.Add(s5);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total students: {0}", Student.count);
+
+            foreach (var student in myList) {
+                Console.WriteLine(student.studentName());
+            }
+            
+        }
+    }
+}
---
+++ Assignment 8/Assignment 8/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7 {
+    class Student {
+        public static int count { get; private set; }
+        //public static Random rnd { get; private set; }
+        private static readonly Random rnd = new Random();
+        public string firstName { private get; set; }
+        public string lastName { private get; set; }
+        public int studentId { private get; set; }
+
+        public Student(string first, string last, int id) {
+            ++count;
+            this.lastName = last;
+            this.firstName = first;
+            this.studentId = id;
+        }
+
+        public Student(string first = "", string last = "") {
+            ++count;
+            this.lastName = last;
+            this.firstName = first;
+            this.studentId = rnd.Next(1000, 9999);
+        }
+
+        public string studentName() {
+            return this.firstName + " " + this.lastName + " " + this.studentId;
+        }
+    }
+}
