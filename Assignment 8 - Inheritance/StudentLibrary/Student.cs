cafe6fec5157f0eb6b551c3ce4e3a7edc118fa31
Daniel Jackson <danj210@hotmail.com>
2017-04-02 14:52:40 -0400

    Did wrong assignment.

--- Assignment 8 - Inheritance/StudentLibrary/Student.cs
+++ Assignment 8 - Inheritance/StudentLibrary/Student.cs
@@ -6,5 +6,24 @@ using System.Threading.Tasks;
 
 namespace StudentLibrary {
     public class Student {
+        public static int Count { get; set; }
+        public static readonly Random rnd;
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int StudentId { get; private set; }
+
+        public Student(string first, string last, int id) {
+            this.FirstName = first;
+            this.LastName = last;
+            this.StudentId = id;
+            Count++;
+        }
+
+        public Student(string first="", string last="") {
+            this.FirstName = first;
+            this.LastName = last;
+            this.StudentId = rnd.Next(1000, 9999);
+        }
     }
 }
