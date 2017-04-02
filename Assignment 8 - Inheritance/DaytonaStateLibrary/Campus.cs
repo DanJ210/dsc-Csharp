7bc793c994010cf14e7a71d4be78f15ee92547ae
Daniel Jackson <danj210@hotmail.com>
2017-04-02 16:02:25 -0400

    Getting down the class libraries as .DLL solution attatments.

---
+++ Assignment 8 - Inheritance/DaytonaStateLibrary/Campus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaytonaStateLibrary {
+    class Campus : DSC {
+        private string campusName;
+        public string departments;
+
+        public Campus(string cName) {
+            this.campusName = cName;
+        }
+
+        public string CampusName() {
+            return campusName;
+        }
+
+        public string Departments() {
+            return "Computer Science Department," +
+                "Emergency Care Department," +
+                "Police Academy";
+        }
+
+        public override string ShowAddress() {
+            return "1770 Williamson Blvd., Daytona Beach" +
+                "Florida 32117";
+        }
+
+        public override string ToString() {
+            return base.SchoolName + CampusName() +
+                " is located at " + ShowAddress() +
+                ", it has " + Departments();
+        }
+    }
+}
---
+++ Assignment 8 - Inheritance/DaytonaStateLibrary/DSC.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaytonaStateLibrary {
+    public class DSC {
+        private string schoolName = "Daytona State College";
+        
+
+        public string SchoolName {
+            get {
+                return schoolName;
+            }
+        }
+
+        public virtual string ShowAddress() {
+            return "1200 W. International Speedway Blvd., " +
+                "Daytona Beach, Florida 32117";
+        }
+    }
+}
