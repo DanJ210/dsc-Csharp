1698d477873a07c5f0464d37d0bcf32b86870ddf
Daniel Jackson <danj210@hotmail.com>
2017-04-12 22:08:06 -0400

    Finished program

--- Assignment 9 - Loan Application/Assignment 9 - Loan Application/Program.cs
+++ Assignment 9 - Loan Application/Assignment 9 - Loan Application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,23 @@ using System.Threading.Tasks;
 namespace Assignment_9___Loan_Application {
     class Program {
         static void Main(string[] args) {
+            double loanAmount;
+            double years;
+            double interestRate;
 
+            Console.WriteLine("Please enter loan amount: ");
+            loanAmount = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter years: ");
+            years = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nPlease enter interest rate: ");
+            interestRate = Convert.ToDouble(Console.ReadLine());
+
+            C1 generateInterest = new C1(loanAmount, years, interestRate);
+            Console.WriteLine($"Total interest rate: {generateInterest.PayInterests()}");
+
+            Console.WriteLine(generateInterest.iMessage());
         }
     }
 }
