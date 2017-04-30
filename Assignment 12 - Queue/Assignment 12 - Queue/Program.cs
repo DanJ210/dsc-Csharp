74d7bbabc652609548b04064b03a439977cd7b44
Daniel Jackson <danj210@hotmail.com>
2017-04-29 20:57:59 -0400

    Adding comments and summaries to code. Final Commit.

--- Assignment 12 - Queue/Assignment 12 - Queue/Program.cs
+++ Assignment 12 - Queue/Assignment 12 - Queue/Program.cs
@@ -6,41 +6,59 @@ using System.Text;
 using System.Threading.Tasks;
 using QueueLibrary;
 
+/* @author Daniel Jackson
+ * @desc Assignment 12 - Queue
+ * 
+ * I made a seperate new project off of memory. I didn't want to copy the stack exactly
+ * but this wasn't difficult to remember so it still looks like a copy, just in Queue form. */
+
 namespace Assignment_12___Queue {
     class Program {
         static void Main(string[] args) {
-            Stack stack = new Stack();
+
+            // Variables to add to the Queue
             bool aBoolean = true;
             int aInteger = 3455;
             string aString = "testString";
             char aCharacter = 'A';
-            
+
             QueueBook newQueueBook = new QueueBook();
             Queue newQueue = newQueueBook.ReturnGeneratedQueue();
+            
+            // Adding to Queue and printing out each result.
+            try {
+                Console.WriteLine("I will add items to a Queue one by one.\n");
 
-            Console.WriteLine("I will add items to a Queue one by one.\n");
+                newQueue.Enqueue(aBoolean);
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Enqueue(aInteger);
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Enqueue(aString);
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Enqueue(aCharacter);
+                newQueueBook.PeekQueue(newQueue);
 
-            newQueue.Enqueue(aBoolean);
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Enqueue(aInteger);
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Enqueue(aString);
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Enqueue(aCharacter);
-            newQueueBook.PrintQueue(newQueue);
+            } catch(Exception ex) {
+                Console.WriteLine("Error creating Queue: {0}", ex.Message);
+            }
+            
+            // Removing from Queue and printing out each result
+            try {
+                Console.WriteLine("\nNow I will give an example of removing from the Queue, one by one.\n");
 
-            Console.WriteLine("\nNow I will give an example of removing from the Queue, one by one.\n");
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Dequeue();
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Dequeue();
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Dequeue();
+                newQueueBook.PeekQueue(newQueue);
+                newQueue.Dequeue();
+            } catch (Exception ex) {
+                Console.WriteLine("Error creating Queue: {0}", ex.Message);
+            }
 
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Dequeue();
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Dequeue();
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Dequeue();
-            newQueueBook.PrintQueue(newQueue);
-            newQueue.Dequeue();
-            //newQueueBook.PrintQueue(newQueue);
-
+            // End of program message
             Console.WriteLine("\nEnd of Queue Example.\n");
         }
     }
---
+++ Assignment 12 - Queue/QueueLibrary/SchoolLibrary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Library that could hold many types of Queue's that I could return in different situations.
+/* I'm using a neat <summary></summary> feature I learned that adds intellisense to these items.
+ * You'll see the intellisense in the other Program.cs file when calling these methods. Hovering
+ * over them shows the intellisense as well. Really neat.
+ */
+
+namespace QueueLibrary {
+    /// <summary>
+    /// Creates an object.
+    /// </summary>
+    public class QueueBook {
+        
+        /// <summary>
+        /// Method that will return a new Queue object for me.
+        /// No need for this except showing that things can be done this way.
+        /// </summary>
+        /// <returns>A new Queue object</returns>
+        public Queue ReturnGeneratedQueue() {
+            return new Queue();
+        }
+ 
+        /// <summary>
+        /// Method to print items inside a Queue
+        /// </summary>
+        /// <param name="que">A Queue from a program</param>
+        public void PeekQueue(Queue que) {
+            if (que.Count != 0) {
+                foreach (var item in que) {
+                    Console.Write("{0} ", item);
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
