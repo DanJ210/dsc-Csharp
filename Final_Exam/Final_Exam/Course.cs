cb677aa61861e87c05180a69c4926c07801647c0
Daniel Jackson <danj210@hotmail.com>
2017-05-08 20:54:35 -0400

    Near complete program.

---
+++ Final_Exam/Final_Exam/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam {
+    class Course {
+        // Private Data Members
+        private string CID;
+        private string CName;
+
+        // Properties
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        // Class Constructor
+        public Course(string courseId, string courseName) {
+            Id = courseId;
+            Name = courseName;
+        }
+    }
+}
---
+++ Final_Exam/Final_Exam/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Exam {
+    class Program {
+        static void Main(string[] args) {
+
+            Course course1 = new Course("CTS1851", "Internet Web Foundation");
+            Course course2 = new Course("CGS2820", "Web Programming");
+            Course course3 = new Course("CGS2821", "Advanced Web Programming");
+            Course course4 = new Course("COP2361", "C# Programming");
+
+            Dictionary<string, Course> Courses = new Dictionary<string, Course>();
+
+            Courses.Add(course1.Id, course1);
+            Courses.Add(course2.Id, course2);
+            Courses.Add(course3.Id, course3);
+            Courses.Add(course4.Id, course4);
+
+            foreach (KeyValuePair<string, Course> kvp in Courses) {
+                Console.WriteLine("The key is {0}, Value is {1}", kvp.Key, kvp.Value.Name);
+            }
+        }
+    }
+}
